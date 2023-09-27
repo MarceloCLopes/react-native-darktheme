8b5d58cb53c64fbae795035af6496ad5d3627c4d
Marcelo Claro <marc01mcl@hotmail.com>
2023-09-27 11:49:44 -0300

    feat: add project

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useColorScheme } from "nativewind";
+import { StyleSheet, Switch, Text, View } from 'react-native';
 
 export default function App() {
+  const { colorScheme, toggleColorScheme } = useColorScheme();
+  console.log(colorScheme);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 justify-center items-center dark:bg-neutral-900 space-y-6">
+      <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
+      <View className="flex-row justify-center items-center space-x-2">
+        <Text className="text-xl dark:text-white">Toggle Theme</Text>
+        <Switch value={colorScheme === "dark"} onChange={toggleColorScheme} />
+      </View>
+      <Text className="mx-4 text-justify" style={colorScheme === 'dark' ? styles.whireText : styles.blackText} >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias ipsam nam saepe? Tempore laudantium ipsa eos iusto consequatur architecto recusandae, quasi praesentium, deserunt dolor, in deleniti et error autem necessitatibus?</Text>
+      <View className="h-48 w-full bg-sky-400 dark:bg-emerald-500" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  whireText: {
+    color: 'white',
   },
-});
+  blackText: {
+    color: 'black',
+  }
+})
+
